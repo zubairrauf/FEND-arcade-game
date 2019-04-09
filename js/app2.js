c193c02854d02532818b11614a2cb073d3779e59
Zubair Rauf <zubairrr@gmail.com>
2019-04-09 14:36:53 +0200

    Add files via upload
    The basic functionality is working. I need to fix the design.

---
+++ js/app2.js
@@ -0,0 +1,214 @@
+//Global variables
+let livesDisplayed = document.getElementsByClassName('life');
+let modal = document.querySelector('.result-modal');
+let speedFactor = 25; //Used to increase the speed from level 6
+
+/****Enemies 1: Cars from left to right*******/
+var Enemy1 = function(x, y) {
+    let randomCar = Math.floor(Math.random() * 6 + 1);
+    let randomSpeed = Math.floor(Math.random() * 15 + 2);
+    this.sprite = 'images/car' +randomCar+'.png';
+    this.x = x;
+    this.y = y;
+    this.update = function(dt) {
+        this.x += randomSpeed * speedFactor * dt;
+        if (this.x > 800){
+            this.x = -150;
+        }
+    };
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy1.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+/****************Enemies1 end *****************/
+
+/***Enemies2: Cars from right to left start****/
+var Enemy2 = function(x, y) {
+    let randomCar = Math.floor(Math.random() * 6 + 1);
+    let randomSpeed = Math.floor(Math.random() * 15 + 1);
+    this.sprite = 'images/car2' +randomCar+'.png';
+    this.x = x;
+    this.y = y;
+    this.update = function(dt) {
+        this.x -= randomSpeed * speedFactor * dt;
+        if (this.x < -100){
+            this.x = 900;
+        }
+    };
+};
+// Draw the enemy on the screen, required method for game
+Enemy2.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+/****************Enemies2 end *****************/
+
+
+/************ Initiate enemies **********/
+let enemy1 = new Enemy1(-200, 330);
+let enemy7 = new Enemy1(-200, 360); 
+let enemy2 = new Enemy1(-150, 250);
+let enemy5 = new Enemy1(-150, 280);
+let enemy4 = new Enemy2(900, 55);
+let enemy6 = new Enemy2(900, 85);
+let enemy3 = new Enemy2(900, 130);
+let enemy8 = new Enemy2(900, 160);
+
+var allEnemies = [enemy1, enemy2, enemy3, enemy4]; //Starting with 4 enemies
+/************** Initiate enemies end  *******/
+
+/*********** Player *******/
+//Player class
+var Player = function() {
+    this.sprite = 'images/walking-monster.png';
+    this.x =350;
+    this.y = 430;
+    this.level = 1;
+    this.lives = 3;
+}
+Player.prototype.update = function(dt){
+    collision();
+    success();    
+};
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+Player.prototype.handleInput = function(dt){
+    if(modal.classList.contains('hidden')){
+        switch (dt){
+        case "up": if(this.y>0){this.y -= 5};
+        break;
+        case "down": if(this.y<460){this.y += 5};
+        break;
+        case "left": if(this.x>0){this.x -= 5};
+        break;
+        case "right": if(this.x<760){this.x += 5};
+        break;
+        }
+    }    
+}
+
+//Initiate player
+var player = new Player();
+/********** Player end ***********/
+
+/******** Gem object start *******/
+var Gem = function(x, y) {
+    let randomGem = Math.floor(Math.random() * 5 + 1);
+    this.sprite = 'images/Gem' +randomGem+'.png';
+    this.x = x;
+    this.y = y;
+    this.update = function(dt) {  }
+    };
+
+// Draw the gems on the screen, required method for game
+Gem.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+var allGems = [new Gem(300,250)];
+/***********Gem object end *******/
+
+
+//Collision detection
+function collision(){
+   // let livesDisplayed = document.getElementsByClassName('life');
+    for (let enemy of allEnemies){
+        let deltaX = player.x - enemy.x;
+        let deltaY = player.y - enemy.y;
+        if (deltaX < 70 && deltaX > -40 && deltaY < 55 && deltaY > -15){
+            console.log(enemy.sprite);
+            player.x = 350;
+            player.y = 430;
+            player.lives -- ;
+            livesDisplayed[player.lives].classList.add('hidden');
+            console.log(player.lives);
+            if(player.lives == 0){
+                lost();
+            }
+        }
+    }
+}
+
+//After three collision, trigger the lost function 
+function lost(){
+    let failureMessage = document.querySelector('.failure-message');
+    modal.classList.remove('hidden');
+    failureMessage.classList.remove('hidden');
+    let againBtn = document.querySelector('.again');
+    againBtn.onclick = function(){
+       modal.classList.add('hidden');
+       failureMessage.classList.add('hidden');
+       player.x = 350;
+       player.y = 430;
+        resetGame();
+    };
+}
+
+function resetGame(){
+    player.level = 1;
+    player.lives = 3;
+    allEnemies = [enemy1, enemy2, enemy3, enemy4];
+    livesDisplayed[0].classList.remove('hidden'); //Refactor it
+    livesDisplayed[1].classList.remove('hidden');
+    livesDisplayed[2].classList.remove('hidden');
+    levelNumber(player.level);
+}
+
+//Success detection
+function success(){
+    if(player.y <40){
+        let modal = document.querySelector('.result-modal');
+        let successMessage = document.querySelector('.success-message');
+        modal.classList.remove('hidden');
+        successMessage.classList.remove('hidden');
+        let nextBtn = document.querySelector('.next');
+        nextBtn.onclick = function(){
+            modal.classList.add('hidden');
+            successMessage.classList.add('hidden');
+            player.x= 350;
+            player.y = 430;
+            nextLevel();            
+        };
+    }
+}
+
+//Increase the difficlty for next levels.
+function nextLevel(){
+    player.level ++;
+    if (player.level == 2){
+        allEnemies.push(enemy5);
+    }
+    else if (player.level == 3){
+        allEnemies.push(enemy6);
+    }
+    else if (player.level == 4){
+        allEnemies.push(enemy7);
+    }
+    else if (player.level == 5){
+        allEnemies.push(enemy8);
+    }
+    else {
+        speedFactor += 5;
+    }
+    levelNumber(player.level); 
+}
+
+//Updates displayed level number
+function levelNumber(playerLevel){
+    let levelNumber = document.querySelector('.level-number');
+    levelNumber.innerText = playerLevel;
+}
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keydown', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
